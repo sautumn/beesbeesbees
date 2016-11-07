b838b94e302137044d195f949a82283a40bd7c49
Autumn Smith <autumn07@gmail.com>
2016-11-07 12:19:01 -0800

    Fixed spacing errors

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,20 +1,23 @@
+var Bee = Bee;
+var Grub = Grub;
+
 var HoneyMakerBee = function() {
-	// call the Bee superclass
-	this = Bee.call(this);
-	// a job property that is set to make honey
-	this.job = "make honey";
-	// an age property that is set to 10
-	this.age = 10;
-	
-	// a color property inherited from bee that is set to yellow
-	//this.color = should be inherited
+  // call the Bee superclass
+  Bee.call(this);
+  // a job property that is set to make honey
+  this.job = 'make honey';
+  // an age property that is set to 10
+  this.age = 10;
 
-	//a honeyPot property that is set to 0
-	this.honeyPot = 0;
+  // a color property inherited from bee that is set to yellow
+  //this.color = should be inherited
 
-	// a food property that is inherited from grub
-	//this.food = should be inherited
-	
+  //a honeyPot property that is set to 0
+  this.honeyPot = 0;
+
+  // a food property that is inherited from grub
+  //this.food = should be inherited
+
 };
 // set the prototype
 HoneyMakerBee.prototype = Object.create(Bee);
@@ -23,25 +26,18 @@ HoneyMakerBee.prototype = Object.create(Bee);
 HoneyMakerBee.constructor = HoneyMakerBee;
 
 // an eat method that is inherited from grub
-HoneyMakerBee.eat = function(){
-	//eat method that is inherited from grub
-	Grub.prototype.eat.call(this);
-}
+HoneyMakerBee.eat = function() {
+  //eat method that is inherited from grub
+  Grub.prototype.eat.call(this);
+};
 
 // a makeHoney method that adds 1 to that honeyBee\'s honeyPot
-HoneyMakerBee.makeHoney = function () {
-	return this.honeyPot += 1;
-}
+HoneyMakerBee.makeHoney = function() {
+  return this.honeyPot += 1;
+};
 
 // a giveHoney method that subtracts 1 from that honeyBee\'s honeyPot
-HoneyMakerBee.giveHoney = function () {
-	return this.honeyPot -= 1;
-}
-// Create a HoneyMakerBee class, in pseudoclassical style, with:
-
-
-
-
-
-
-
+HoneyMakerBee.giveHoney = function() {
+  return this.honeyPot -= 1;
+};
+  // Create a HoneyMakerBee class, in pseudoclassical style, with:
