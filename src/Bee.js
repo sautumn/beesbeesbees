a34dbfb3d9f7a5d1562dfc2a9a2bce033adf8feb
Autumn Smith <autumn07@gmail.com>
2016-11-07 12:14:47 -0800

    fixing pomade errors

--- src/Bee.js
+++ src/Bee.js
@@ -1,12 +1,13 @@
+var Grub = Grub;
 var Bee = function() {
-	// call the Grub superclass
-	this = Grub.call(this);
-	// an age property that is set to 5
-	this.age = 5;
-	// a color property that is set to yellow
-	this.color = 'yellow';
-	// a job property that is set to keep on growing
-	this.job = 'keep on growing';
+  // call the Grub superclass
+  Grub.call(this);
+  // an age property that is set to 5
+  this.age = 5;
+  // a color property that is set to yellow
+  this.color = 'yellow';
+  // a job property that is set to keep on growing
+  this.job = 'keep on growing';
 };
 
 //set the prototype
@@ -15,14 +16,11 @@ Bee.prototype = Object.create(Grub);
 // set the constructor
 Bee.constuctor = Bee;
 
-Bee.prototype.food = function () {
-	return this.food;
-}
+Bee.prototype.food = function() {
+  return this.food;
+};
 
 // an eat method that is inherited from grub
-Bee.prototype.eat = function () {
-	return Grub.prototype.eat;
-}
-
-
-
+Bee.prototype.eat = function() {
+  return Grub.prototype.eat;
+};
