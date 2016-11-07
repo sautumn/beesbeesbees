833070483dae8f762ab72811f803f5def1084908
Autumn Smith <autumn07@gmail.com>
2016-11-07 12:21:14 -0800

    Fixed spacing errors

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,26 +1,29 @@
+var ForagerBee = ForagerBee;
+var Bee = Bee;
+
 var RetiredForagerBee = function() {
-	// call the ForagerBee superclass
-	this = ForagerBee.call(this);
+  // call the ForagerBee superclass
+  ForagerBee.call(this);
 
-	// an age property that is set to 40
-	this.age = 40;
-	// a job property that is set to gamble
-	this.job = 'gamble';
-	// a canFly property that is set to false
-	this.canFly = false;
-	// a color property that is set to grey
-	this.color = 'grey';
-	// a forage method that returns "I am too old, let me play cards instead"
-	this.forage = "I am too old, let me play cards instead";
+  // an age property that is set to 40
+  this.age = 40;
+  // a job property that is set to gamble
+  this.job = 'gamble';
+  // a canFly property that is set to false
+  this.canFly = false;
+  // a color property that is set to grey
+  this.color = 'grey';
+  // a forage method that returns "I am too old, let me play cards instead"
+  this.forage = 'I am too old, let me play cards instead';
 
-	// a food property that is inherited from grub
-	//this.food should go up chain to get the grub
+  // a food property that is inherited from grub
+  //this.food should go up chain to get the grub
 
-	// an eat method that is inherited from grub
-	//this.eat should go up the chain to grub
+  // an eat method that is inherited from grub
+  //this.eat should go up the chain to grub
 
-	// a treasureChest property inherited from ForagerBee that is set to an empty array []
-	this.treasureChest = [];
+  // a treasureChest property inherited from ForagerBee that is set to an empty array []
+  this.treasureChest = [];
 
 };
 
@@ -31,6 +34,6 @@ RetiredForagerBee.prototype = Object.create(Bee.prototype);
 RetiredForagerBee.constructor = RetiredForagerBee;
 
 // an always winning gamble method that allows the bee to add a treasure to the treasureChest
-RetiredForagerBee.prototype.gamble = function (treasure) {
-	this.treasureChest.push(treasure);
-}
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+};
