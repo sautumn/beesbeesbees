0af8c60c712cb199fce17d80600176691e63e3a0
Autumn Smith <autumn07@gmail.com>
2016-11-07 12:22:28 -0800

    fixed spacing errors

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,22 +1,24 @@
+var Bee = Bee;
+
 var ForagerBee = function() {
-	// call the Bee superclass
-	this = Bee.call(this);
-	// an age property that is set to 10
-	this.age = 10;
-	// a job property that is set to find pollen
-	this.job = 'find pollen';
-	this.honeypot = 0;
+  // call the Bee superclass
+  Bee.call(this);
+  // an age property that is set to 10
+  this.age = 10;
+  // a job property that is set to find pollen
+  this.job = 'find pollen';
+  this.honeypot = 0;
 
-	// a canFly property that is set true
-	this.canFly = true;
+  // a canFly property that is set true
+  this.canFly = true;
 
-	this.treasureChest = [];
+  this.treasureChest = [];
 
-	// a color property inherited from bee that is set to yellow
-	//this.color should inherit from the bee
+  // a color property inherited from bee that is set to yellow
+  //this.color should inherit from the bee
 
-	// a food property that is inherited from grub
-	//this.food should go up to prototype chain to get food from grub
+  // a food property that is inherited from grub
+  //this.food should go up to prototype chain to get food from grub
 
 };
 
@@ -27,9 +29,7 @@ ForagerBee.constructor = ForagerBee;
 ForagerBee.prototype = Object.create(Bee);
 
 // a forage method that allows the bee to add a treasure to the treasureChest
-ForagerBee.prototype.forage = function(value){
-	//add to the treasechest
-	this.treasureChest.push(value);
-}
-
-
+ForagerBee.prototype.forage = function(value) {
+  //add to the treasechest
+  this.treasureChest.push(value);
+};
