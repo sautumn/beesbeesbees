7b732e3d3748c6ce90139cf206eaf40e143d979c
Autumn Smith <autumn07@gmail.com>
2016-11-07 12:24:28 -0800

    fixed spacing errors

--- src/Grub.js
+++ src/Grub.js
@@ -1,16 +1,14 @@
 var Grub = function() {
-	// an age property that is set to 0
-	this.age = 0;
-	// a color property that is set to pink
-	this.color = 'pink';
-	// a food property that is set to jelly
-	this.food = 'jelly';
+  // an age property that is set to 0
+  this.age = 0;
+  // a color property that is set to pink
+  this.color = 'pink';
+  // a food property that is set to jelly
+  this.food = 'jelly';
 };
 
 // an eat method
 Grub.prototype.eat = function() {
-	//eat method
-	return this.food;
+  //eat method
+  return this.food;
 };
-
-
